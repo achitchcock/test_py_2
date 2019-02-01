b2411d30bb03c8b182c9a9ee1a1fd40f32c77713
Aaron Hitchcock <aaron.hitchcock.ext@nokia.com>
2019-01-31 16:34:05 -0800

    NxN spiral problem

---
+++ NxNspiral.py
@@ -0,0 +1,39 @@
+
+class NxNspiral:
+    def __init__(self, size):
+        self.size = size
+        self.data = [[None for x in range(size)] for y in range(size)]
+
+    def diplay(self):
+        fmt_str = "{: >" + str(len(str(self.size ** 2))) + "}"
+        for line in self.data:
+            print ("[" + (fmt_str + ", ") * (self.size-1) + fmt_str + "]").format(*line)
+
+    def fill_spiral(self):
+        count = 1
+        x = -1
+        y = 0
+        dx = 1
+        dy = 1
+        hr = self.size
+        vr = self.size - 1
+        while count <= self.size**2:
+            for hi in range(hr):
+                x += dx
+                self.data[y][x] = count
+                count += 1
+            hr -= 1
+            dx *= -1
+            for vi in range(vr):
+                y += dy
+                self.data[y][x] = count
+                count += 1
+            vr -= 1
+            dy *= -1
+
+
+for i in [10,20]:  # range(1,10):
+    mySpiral = NxNspiral(i)
+    mySpiral.fill_spiral()
+    mySpiral.diplay()
+    print
